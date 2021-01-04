7597fa6cadb24f93c144a4ee6030bfaa0c72c974
Frans Korhonen <frans.korhonen@rmcios.fi>
2021-01-04 23:20:37 +0200

    utilities: Add mkdir command variable

--- utilities.mk
+++ utilities.mk
@@ -3,10 +3,12 @@ ifeq ($(OS),Windows_NT)     # is Windows_NT on XP, 2000, 7, Vista, 10...
    COPY:=copy
    /:=\\
    SHELL=cmd
+   MKDIR:=mkdir
 else
    COPY:=cp
    COPY_DIR:=cp -r
    /:=/
+   MKDIR:=mkdir -p
 endif
 
 
