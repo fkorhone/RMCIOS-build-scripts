c927de12dd81c6e9cb52df9714c5784fb42079c1
Frans Korhonen <frans.korhonen@rmcios.fi>
2021-01-05 08:53:47 +0200

    Fix directory separator variable in windows

--- utilities.mk
+++ utilities.mk
@@ -1,13 +1,14 @@
+
+/:=/
 ifeq ($(OS),Windows_NT)     # is Windows_NT on XP, 2000, 7, Vista, 10...
    COPY_DIR:=xcopy /E /I
    COPY:=copy
-   /:=\\
+   /:=$(subst /,\,$/)
    SHELL=cmd
    MKDIR:=mkdir
 else
    COPY:=cp
    COPY_DIR:=cp -r
-   /:=/
    MKDIR:=mkdir -p
 endif
 
